4c3841b6137fcd0e49065dccca1ec717d8e0abd6
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-07-18 23:00:16 -0300

    update: function to save host in storage in HostModel

--- src/app/models/HostModel.js
+++ src/app/models/HostModel.js
@@ -1,11 +1,43 @@
+const electronStore = require('electron-store');
+const store = new electronStore();
 
-class Host{
-    constructor(name,username,address){
-        this.name = name;
-        this._username = username;
-        this._address = address;
+
+class HostModel{
+    
+    constructor(name_id, username_id, address_id, password_id) {
+        this.name = document.querySelector(`#${name_id}`).value;
+        this._username = document.querySelector(`#${username_id}`).value;
+        this._address = document.querySelector(`#${address_id}`).value;
+        this._password = document.querySelector(`#${password_id}`).value;
+
+        this._saveInStorage();
     }
-     get infos(){
-         return `${this.name},${this._username}, ${this._address}`;
-     }
+
+    get infos() {
+        return `${this.name},${this._username}, ${this._address}`;
+    }
+
+    _saveInStorage() {
+
+        const hostWithoutPass = {
+            name: this.name,
+            username: this._username,
+            address: this._address,
+            infos:()=>{
+                return `${name} em ${username}@${address}`
+            }
+        }
+
+        if (!store.get('storage-hosts')) {
+            const hosts_storage = new Array();
+            hosts_storage.push(hostWithoutPass);
+            store.set('storage-hosts', hosts_storage);
+        } else {
+            const hosts = store.get('storage-hosts');
+            hosts.push(hostWithoutPass);
+            store.set('storage-hosts', hosts);
+
+        }
+    }
+
 }
